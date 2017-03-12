6a27fd6eceba119c67f64e12be0b2e5f0a0b172d
Surdu Petru <surdu_petru@yahoo.com>
2017-03-12 11:45:16 +0100

    update to N

--- frd-vendor-blobs.mk
+++ frd-vendor-blobs.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,3 +14,6 @@
 
 PRODUCT_COPY_FILES += \
 	$(call find-copy-subdir-files,*,vendor/huawei/frd/proprietary/,system/)
+
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,vendor/huawei/frd/root/,root/)
