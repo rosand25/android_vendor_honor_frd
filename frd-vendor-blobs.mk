f09e800e10ba52045a586bad25a2330a341297d5
Huawei-Dev <surdu_petru@yahoo.com>
2017-04-14 23:55:13 +0200

    switch huawei to honor

--- frd-vendor-blobs.mk
+++ frd-vendor-blobs.mk
@@ -13,4 +13,4 @@
 # limitations under the License.
 
 PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,vendor/huawei/frd/proprietary/,system/)
+	$(call find-copy-subdir-files,*,vendor/honor/frd/proprietary/,system/)
